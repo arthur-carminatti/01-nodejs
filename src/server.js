69a77da18f221ca1cd2437e495bad9d74c4969b7
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-05-12 13:48:14 -0300

    creating read stream

--- src/server.js
+++ src/server.js
@@ -17,10 +17,10 @@ const server = http.createServer((req, res) => {
             name: 'Jhon Doe',
             email: 'example@gmail.com'
         })
-        return res.end('Criação de usuário')
+        return res.writeHead(201).end('')
     }
 
-    return res.end('Hello World')
+    return res.writeHead(404).end()
 })
 
 server.listen(3333)
