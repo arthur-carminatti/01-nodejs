30b5e4cb2785ffe5a7be1c415a5333e2a1eeb5ad
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-05-12 11:28:23 -0300

    saving user in memory

--- src/server.js
+++ src/server.js
@@ -1,6 +1,25 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
+    const { method, url } = req
+
+    if (method === 'GET' && url === '/users') {
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
+    }
+
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'Jhon Doe',
+            email: 'example@gmail.com'
+        })
+        return res.end('Criação de usuário')
+    }
+
     return res.end('Hello World')
 })
 
